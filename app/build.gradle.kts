50004beb5cc9a6aae6bbe25ec247f4f2eff6fdc7
MY-moss <137242245@qq.com>
2026-09-02 08:10:12 +0800

    chore: prepare Learn List v0.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.mymoss.learnlist"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.3.3"
+        versionCode = 17
+        versionName = "0.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
