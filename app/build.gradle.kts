05f8fef7c64a8989cc96accb7da95347255c3232
墨阳 <137242245@qq.com>
2026-09-02 10:41:59 +0800

    chore: prepare Learn List v0.3.7 (#92)
    Bump the Android version for the accessible collapsible header release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.mymoss.learnlist"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.3.6"
+        versionCode = 20
+        versionName = "0.3.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
